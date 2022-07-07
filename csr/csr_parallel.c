587f7df38eb0b56c12133db5276b8340aec3a889
ArshiAAkhavan <letmemakenewone@gmail.com>
2022-07-07 11:33:36 +0430

    add timings for csr_parallel.c

--- csr/csr_parallel.c
+++ csr/csr_parallel.c
@@ -4,17 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/sysinfo.h>
+#include <sys/time.h>
 #include <time.h>
-
-#ifndef ROW_SIZE
-#define ROW_SIZE 5
-#endif /* !ROW_SIZE */
-
-#ifndef COL_SIZE
-#define COL_SIZE 3
-#endif /* !COL_SIZE */
-
-#define NUM_THREADS 16
 
 // csr data format for storing matrices
 typedef struct CSR {
@@ -38,10 +29,9 @@ void csr_from_raw(int **matrix, size_t row_size, size_t col_size, csr_t *csr) {
   // construct CSR
   csr->cols = malloc(sizeof(int) * nnz);
   csr->vals = malloc(sizeof(int) * nnz);
-  csr->row_ptrs = malloc(sizeof(int) * (col_size + 1));
+  csr->row_ptrs = malloc(sizeof(size_t) * (col_size + 1));
   csr->nnz = nnz;
   csr->col_size = col_size;
-  printf("im here\n");
 
   // fill the CSR vectors
   size_t counter = 0;
@@ -70,54 +60,69 @@ void csr_mul_parallel(csr_t *csr, int *vector, int *output) {
   }
 }
 
-int main() {
+int main(int argc, char **argv) {
+  srand(time(NULL));
+  if (argc < 3)
+    return -1;
+  int col_size = atoi(argv[1]);
+  int row_size = atoi(argv[2]);
+
   omp_set_num_threads(get_nprocs()); // TODO: set this to 1, 4, 16, and 64
 
 #pragma omp parallel
   { volatile char sync_threads = 0; }
 
-  srand(time(NULL));
-  int *matrix[COL_SIZE];
-  int vector[ROW_SIZE];
-  int output[COL_SIZE];
+  int *matrix[col_size];
+  int vector[row_size];
+  int output[col_size];
   csr_t csr;
 
-  fill_vector(vector, ROW_SIZE, 0.9f);
-  fill_matrix(matrix, ROW_SIZE, COL_SIZE, 0.5f);
-  csr_from_raw(matrix, ROW_SIZE, COL_SIZE, &csr);
+  fill_vector(vector, row_size, 0.9f);
+  fill_matrix(matrix, row_size, col_size, 0.5f);
+  csr_from_raw(matrix, row_size, col_size, &csr);
+
+  struct timeval t1, t2;
+  double elapsedTime;
+
+  gettimeofday(&t1, NULL);
   csr_mul_parallel(&csr, vector, output);
+  gettimeofday(&t2, NULL);
 
-  printf("Matrix is:\n");
-  for (size_t i = 0; i < COL_SIZE; i++, printf("\n"))
-    for (size_t j = 0; j < ROW_SIZE; j++)
-      printf("%d ", matrix[i][j]);
-  printf("\n");
+  elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;    // sec to ms
+  elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0; // us to ms
+  printf("time: %lf\n", elapsedTime);
 
-  printf("Vector is:\n");
-  for (size_t j = 0; j < ROW_SIZE; j++)
-    printf("%d ", vector[j]);
-  printf("\n");
-
-  printf("\n");
-  printf("csr format is:\n");
-  printf("r_ptrs:\t");
-  for (size_t j = 0; j < csr.col_size + 1; j++)
-    printf("%ld ", csr.row_ptrs[j]);
-
-  printf("\ncols:\t");
-  for (size_t j = 0; j < csr.nnz; j++)
-    printf("%d ", csr.cols[j]);
-
-  printf("\nvals:\t");
-  for (size_t j = 0; j < csr.nnz; j++)
-    printf("%d ", csr.vals[j]);
-  printf("\n");
-
-  printf("\n");
-  printf("Output is:\n");
-  for (size_t j = 0; j < COL_SIZE; j++)
-    printf("%d ", output[j]);
-  printf("\n");
-
+  // printf("Matrix is:\n");
+  // for (size_t i = 0; i < col_size; i++, printf("\n"))
+  //   for (size_t j = 0; j < row_size; j++)
+  //     printf("%d ", matrix[i][j]);
+  // printf("\n");
+  //
+  // printf("Vector is:\n");
+  // for (size_t j = 0; j < row_size; j++)
+  //   printf("%d ", vector[j]);
+  // printf("\n");
+  //
+  // printf("\n");
+  // printf("csr format is:\n");
+  // printf("r_ptrs:\t");
+  // for (size_t j = 0; j < csr.col_size + 1; j++)
+  //   printf("%ld ", csr.row_ptrs[j]);
+  //
+  // printf("\ncols:\t");
+  // for (size_t j = 0; j < csr.nnz; j++)
+  //   printf("%d ", csr.cols[j]);
+  //
+  // printf("\nvals:\t");
+  // for (size_t j = 0; j < csr.nnz; j++)
+  //   printf("%d ", csr.vals[j]);
+  // printf("\n");
+  //
+  // printf("\n");
+  // printf("Output is:\n");
+  // for (size_t j = 0; j < col_size; j++)
+  //   printf("%d ", output[j]);
+  // printf("\n");
+  //
   return 0;
 }
